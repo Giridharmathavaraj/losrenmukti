f41befee7b10b76b82bcf44dcb8aa69b36bb4036
Giridharmathavaraj <giridharmathavaraj@gmail.com>
2026-03-31 18:25:25 +0530

    Deployment: Finished Catalyst configuration for static Slate export and function sync

--- functions/server/index.js
+++ functions/server/index.js
@@ -756,5 +756,9 @@ app.use((req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`🚀 Server running locally on port ${PORT}`));
+}
+
+export default app;
